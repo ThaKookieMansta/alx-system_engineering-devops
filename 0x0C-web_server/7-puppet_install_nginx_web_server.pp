940ba9b3410e89bb5d00cdf9f8dc5b39db830f77
Chris Mukirae <chriskagiko@gmail.com>
2023-09-25 11:28:51 +0300

    Complete Project This commit is after completion of the project

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,79 @@
+# Add Nginx repository for download and install
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# Update packages for latest stable versions and dependancies
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# install nginx web server
+package { 'nginx':
+  ensure     => 'installed',
+}
+
+# allow HTTP on port 80
+exec { 'allow HTTP':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
+}
+
+# Modify directory rights on root directory of nginx
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# Create landing page with index.html
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
+
+# Create 404 error page
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+# Configure the redirection
+file { 'Nginx default config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        # Add index.php to the list if you are using PHP
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
+}
+",
+}
+# Restart Nginx service
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+# start service Nginx
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
---
+++ 0x0C-web_server/README.md
@@ -0,0 +1,3 @@
+# 0x0C-web_server
+
+The content of this directory is for the 0x0C-web_server ALX project
